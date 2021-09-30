d78881aeb061686b0f22227192e482dea692f2e7
dorcsyful <74967930+dorcsyful@users.noreply.github.com>
2021-09-30 11:17:00 +0200

    Basic functions in CCollisionObject

--- CinkesCollision/CCollisionObject.cpp
+++ CinkesCollision/CCollisionObject.cpp
@@ -1,3 +1,75 @@
 
 // ReSharper disable once CppPrecompiledHeaderIsNotIncluded
 #include "CCollisionObject.h"
+
+Cinkes::CCollisionObject::CCollisionObject()
+{
+	m_Shape = nullptr;
+	m_Transform = CTransform();
+}
+
+Cinkes::CCollisionObject::CCollisionObject(const CTransform& a_Transform, CCollisionShape* a_Shape)
+{
+	m_Transform = a_Transform;
+	m_Shape = a_Shape;
+}
+
+Cinkes::CCollisionObject::~CCollisionObject()
+{
+	m_Shape = nullptr;
+}
+
+Cinkes::CCollisionObject::CCollisionObject(const CCollisionObject& a_Rhs)
+{
+	m_Transform = a_Rhs.m_Transform;
+	m_Shape = a_Rhs.m_Shape;
+}
+
+Cinkes::CCollisionObject::CCollisionObject(CCollisionObject&& a_Rhs) noexcept 
+{
+	m_Transform = a_Rhs.m_Transform;
+	m_Shape = a_Rhs.m_Shape;
+}
+
+Cinkes::CCollisionObject& Cinkes::CCollisionObject::operator=(CCollisionObject&& a_Rhs)
+{
+	m_Transform = a_Rhs.m_Transform;
+	m_Shape = a_Rhs.m_Shape;
+
+	return *this;
+}
+
+Cinkes::CCollisionObject& Cinkes::CCollisionObject::operator=(const CCollisionObject& a_Rhs)
+{
+	if(&a_Rhs == this)
+	{
+		return *this;
+	}
+	m_Transform = a_Rhs.m_Transform;
+	m_Shape = a_Rhs.m_Shape;
+
+	return *this;
+}
+
+void Cinkes::CCollisionObject::SetCollisionShape(CCollisionShape* a_Shape)
+{
+	if(m_Shape != nullptr)
+	{
+		m_Shape = a_Shape;
+	}
+}
+
+Cinkes::CCollisionShape* Cinkes::CCollisionObject::GetCollisionShape() const
+{
+	return m_Shape;
+}
+
+void Cinkes::CCollisionObject::SetTransform(const CTransform& a_Transform)
+{
+	m_Transform = a_Transform;
+}
+
+Cinkes::CTransform& Cinkes::CCollisionObject::GetTransform()
+{
+	return m_Transform;
+}
--- CinkesCollision/CCollisionObject.h
+++ CinkesCollision/CCollisionObject.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include "../CinkesMath/CTransform.h"
 namespace Cinkes
 {
 	class CCollisionShape;
@@ -9,23 +9,30 @@ namespace Cinkes
 	public:
 		//Subgroup: Constructors {
 		CCollisionObject();
+		explicit CCollisionObject(const CTransform& a_Transform, CCollisionShape* a_Shape = nullptr);
 		~CCollisionObject();
 		CCollisionObject(const CCollisionObject& a_Rhs);
-		CCollisionObject(CCollisionObject&& a_Rhs);
+		CCollisionObject(CCollisionObject&& a_Rhs) noexcept; 
 		//}
 
 		//Subgroup: Operators {
-		CCollisionObject& operator=(CCollisionObject&& a_Rhs);
+		CCollisionObject& operator=(CCollisionObject&& a_Rhs) noexcept;
 		CCollisionObject& operator=(const CCollisionObject& a_Rhs);
 		//}
 
 		//Subgroup: Shapes {
 		void SetCollisionShape(CCollisionShape* a_Shape);
-		CCollisionShape* GetCollisionShape();
+		CCollisionShape* GetCollisionShape() const;
+		//}
+
+		//Subgroup: Transform {
+		void SetTransform(const CTransform& a_Transform);
+		CTransform& GetTransform();
 		//}
 
 	private:
 		CCollisionShape* m_Shape;
+		CTransform m_Transform;
 	};
 
 }
